0e5ca77151701f6510f2adbf5e1b5060a5d31049
HizbullahBhatti <hizbullah.kcsf18@iba-suk.edu.pk>
2023-02-20 20:15:13 +0500

    Final Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# FYP-Webpage
+To connect your Jackal Robot to a webpage, you can use the ROS Bridge server to establish a connection between your robot and a web client, such as a webpage. Here are the high-level steps you can follow to connect your Jackal Robot to a webpage:
+
+Install and run the ROS Bridge server on your robot: The ROS Bridge server provides a websockets interface to communicate with ROS. You can install it by running the command sudo apt-get install ros-<ros-version>-rosbridge-server on your Jackal Robot. Once installed, you can run the ROS Bridge server by running the command roslaunch rosbridge_server rosbridge_websocket.launch.
+
+Write a web client using a web development framework: You can use a web development framework such as React, Angular, or Vue to write a web client that can communicate with the ROS Bridge server. In your web client, you can use the roslibjs library to communicate with ROS topics and services.
+
+Establish a connection between the web client and the ROS Bridge server: In your web client, you can use the roslibjs library to establish a connection to the ROS Bridge server by providing the ROS Bridge address, which is usually in the format ws://<robot-ip>:9090.
+
+Publish and subscribe to ROS topics and services from your web client: Once the connection is established, you can use the roslibjs library to publish and subscribe to ROS topics and services, which will enable you to perform operations with your Jackal Robot from the webpage.
+
+Here is a high-level example of how you can control the movement of the Jackal Robot using a webpage:
+
+In your web client, create buttons or input fields for controlling the movement of the robot, such as "forward", "backward", "left", and "right".
+
+When a button is clicked or a value is entered in the input field, use the roslibjs library to publish a message to a ROS topic that controls the robot's movement.
+
+In the ROS node running on the Jackal Robot, subscribe to the ROS topic that controls the robot's movement, and send the appropriate commands to the robot's motors to move it in the desired direction.
+
+With this setup, you can control the Jackal Robot's movement from a webpage. However, keep in mind that you will need to have a good understanding of ROS, web development, and the roslibjs library to implement this solution.
+
+
+roslaunch rosbridge_server rosbridge_websocket.launch
+rosnode list
+roslaunch rosbridge_server rosbridge_websocket.launch
--- main.js
+++ main.js
@@ -1,9 +1,222 @@
 let vueApp = new Vue({
     el: "#vueApp",
     data: {
-        menu_title: 'My menu title',
-        main_title: 'Main title, from Vue!!',
-        p_content_01: 'This is the left side of my web page. It occupies 33% of the total width',
-        p_content_02: 'Here it goes the main content of my first web page, able to control ROS robots using web tools!',
-    }
+        // ros connection
+        ros: null,
+        rosbridge_address: 'ws://localhost:9090/',
+        connected: false,
+        // page content
+        menu_title: 'Connection',
+        main_title: 'Move the Jackal',
+        // subscriber data
+        position: { x: 0, y: 0, z: 0, },
+        // fence mode 
+        fenceMode: false,
+        insideFence: false, 
+        // dragging data
+        dragging: false,
+        x: 'no',
+        y: 'no',
+        dragCircleStyle: {
+            margin: '0px',
+            top: '0px',
+            left: '0px',
+            display: 'none',
+            width: '75px',
+            height: '75px',
+    },
+    // joystick valules
+    joystick: {
+        vertical: 0,
+        horizontal: 0,
+    },
+    // publisher
+    pubInterval: null,
+},
+    methods: {
+        connect: function() {
+            // define ROSBridge connection object
+            this.ros = new ROSLIB.Ros({
+                url: this.rosbridge_address
+            })
+
+            // define callbacks
+            this.ros.on('connection', () => {
+                this.connected = true
+                console.log('Connection to ROSBridge established!')
+                let topic = new ROSLIB.Topic({
+                    ros: this.ros,
+                    name: '/jackal_velocity_controller/odom',
+                    messageType: 'nav_msgs/Odometry'
+                })
+                topic.subscribe((message) => {
+                    this.position = message.pose.pose.position
+                    console.log(`fence mode is ${this.fenceMode}`)
+                    if (this.fenceMode) {
+                        this.stayOnTheFence(message.pose.pose.position)
+                    }
+                })
+            })
+            this.ros.on('error', (error) => {
+                console.log('Something went wrong when trying to connect')
+                console.log(error)
+            })
+            this.ros.on('close', () => {
+                this.connected = false
+                console.log('Connection to ROSBridge was closed!')
+                clearInterval(this.pubInterval)
+            })
+            this.ros.on('connection', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Connected!')
+                this.connected = true
+                this.loading = false
+                this.setCamera()
+            })
+            this.ros.on('close', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Disconnected!')
+                this.connected = false
+                this.loading = false
+                document.getElementById('divCamera').innerHTML = ''
+            })
+        },
+        publish: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: this.joystick.vertical, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: this.joystick.horizontal, },
+            })
+            topic.publish(message)
+        },
+        //Camera Function
+        setCamera: function() {
+            let without_wss = this.rosbridge_address.split('wss://localhost:9090')[1]
+            console.log(without_wss)
+            let domain = without_wss.split('/')[0] + '/' + without_wss.split('/')[1]
+            console.log(domain)
+            let host = domain + '/cameras'
+            let viewer = new MJPEGCANVAS.Viewer({
+                divID: 'divCamera',
+                host: host,
+                width: 320,
+                height: 240,
+                topic: '/camera/rgb/image_raw',
+                ssl: true,
+            })
+        },
+        disconnect: function() {
+            this.ros.close()
+        },
+        sendCommand: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: 10, y: 0, z: 0, },
+                //angular: { x: 0, y: 0, z: 0.5, },
+            })
+            topic.publish(message)
+        
+        },
+        //Camera Function Ends here
+        startDrag() {
+            this.dragging = true
+            this.x = this.y = 0
+        },
+        stopDrag() {
+            this.dragging = false
+            this.x = this.y = 'no'
+            this.dragCircleStyle.display = 'none'
+            this.resetJoystickVals()
+        },
+        doDrag(event) {
+            if (this.dragging) {
+                this.x = event.offsetX
+                this.y = event.offsetY
+                let ref = document.getElementById('dragstartzone')
+                this.dragCircleStyle.display = 'inline-block'
+
+                let minTop = ref.offsetTop - parseInt(this.dragCircleStyle.height) / 2
+                let maxTop = minTop + 200
+                let top = this.y + minTop
+                this.dragCircleStyle.top = `${top}px`
+
+                let minLeft = ref.offsetLeft - parseInt(this.dragCircleStyle.width) / 2
+                let maxLeft = minLeft + 200
+                let left = this.x + minLeft
+                this.dragCircleStyle.left = `${left}px`
+
+                this.setJoystickVals()
+            }
+        },
+        setJoystickVals() {
+            this.joystick.vertical = -1 * ((this.y / 200) - 0.5)
+            this.joystick.horizontal = +1 * ((this.x / 200) - 0.5)
+        },
+        resetJoystickVals() {
+            this.joystick.vertical = 0
+            this.joystick.horizontal = 0
+        },
+        turnRight: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: 0, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 5, },
+            })
+            topic.publish(message)
+        },
+        stop: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: 0, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            topic.publish(message)
+        },
+        switchFenceMode: function() {
+            this.fenceMode = !this.fenceMode
+        },
+        stayOnTheFence: function(position) {
+            let topicToPublish = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            if (position.x > -0.5 && position.x < 0.5 && position.y > -0.5 && position.y < 0.5) {
+                // we are inside the fence!
+                this.insideFence = true
+                let message = new ROSLIB.Message({
+                    linear: { x: 0.5, y: 0, z: 0, },
+                    angular: { x: 0, y: 0, z: 0, },
+                })
+                topicToPublish.publish(message)
+            } else {
+                // we are outside the fence!
+                this.insideFence = false
+                let message = new ROSLIB.Message({
+                    linear: { x: 0.5, y: 0, z: 0, },
+                    angular: { x: 0, y: 0, z: 0.5, },
+                })
+                topicToPublish.publish(message)
+            }
+        },
+    },
+    mounted() {
+        // page is ready
+        console.log('page is ready!')
+        window.addEventListener('mouseup', this.stopDrag)
+    },
 })
